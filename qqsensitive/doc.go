5eec7a15ced0b08b978af8fc32cf3c2beae7f481
YangQi <289119467@qq.com>
2023-10-17 17:37:26 +0800

    Initial commit

---
+++ qqsensitive/doc.go
@@ -0,0 +1,7 @@
+package qqsensitive
+
+// qqsensitive包提供敏感词单模词和多模词匹配
+// dict := "./dict.txt"
+// dictMulti := "./dictMulti.txt"
+// 使用方法 sens, err := qqsensitive.NewSensitive(dict, dictMulti)
+// sens.Check(content)
