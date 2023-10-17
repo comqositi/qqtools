703b143e4c017565b03a041b9e8c831dba8510ce
YangQi <289119467@qq.com>
2023-10-17 18:10:06 +0800

    Initial commit

--- qqsensitive/sensitive_test.go
+++ qqsensitive/sensitive_test.go
@@ -5,8 +5,8 @@ import "testing"
 func TestSensitive_Check(t *testing.T) {
 	content := "你知道白纸事件是什么情况吗"
 	// dict相对路劲应为可执行文件路劲的相对位置
-	dict := "../resource/sensitive_zz_sq.txt"
-	dictMulti := "../resource/sensitive_zz_sq_multi.txt"
+	dict := "./resource/sensitive_zz_sq.txt"
+	dictMulti := "./resource/sensitive_zz_sq_multi.txt"
 	sens, err := NewSensitive(dict, dictMulti)
 	if err != nil {
 		t.Error(err)
